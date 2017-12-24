1d6aa0fcd1667159a9f617579859de60e222b1db
Giel Cobben <g.cobben@gmail.com>
2017-12-24 15:29:01 +0100

    ready for review

--- examples/search.js
+++ examples/search.js
@@ -1,5 +1,5 @@
 const Core = require("./../dist/index");
 
-Core.searchByQuery("Vikings", "eng", 10)
+Core.searchByQuery("Vikings ", "eng", 10)
   .on("fastest", results => console.log("Fastest results", results))
   .on("completed", results => console.log("All results", results));
